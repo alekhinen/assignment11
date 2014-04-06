766e9a5b8ea7c1b0a34f2db0cf8b89eaf7dbb782
Austin Colcord <ajdcolcord@gmail.com>
2014-04-06 16:42:00 -0400

    updated gradebook constructor

--- src/GradeBook.java
+++ src/GradeBook.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /** represents the grade book for each course that an instructor teaches
  * There can be multiple gradebooks for one instructor
@@ -11,14 +12,29 @@ import java.util.ArrayList;
  */
 public class GradeBook {
     String courseName;
-    ArrayList<Assignment> assignList;
-    ArrayList<Student> studList;
+    HashMap<Student, ArrayList<Assignment>> studAssignMap;
     
-    GradeBook(String courseName,
-            ArrayList<Assignment> assignList,
-            ArrayList<Student> studList) {
+    GradeBook(String courseName, 
+            HashMap<Student, ArrayList<Assignment>> studAssignMap) {
         this.courseName = courseName;
-        this.assignList = new ArrayList<Assignment>();
-        this.studList = new ArrayList<Student>();
+        this.studAssignMap = new HashMap<Student, ArrayList<Assignment>>();
+    }
+    
+    /**
+     * Changes the assignment (named assignmentName) grade for student (whose
+     * username is equal to username) to newGrade
+     * 
+     * @param assignmentName
+     *            name of the assignment
+     * @param username
+     *            username for the student
+     * @param newGrade
+     *            the new grade for the given assignment and student
+     * @return whether there was a grade to change. Returns true if the given
+     *         assignment/student combination exists, returns false otherwise
+     */
+    public boolean changeGrade(String assignmentName,
+            String username, double newGrade) {
+        
     }
 }
