255939f99abe051e59fd7e74168772691acfad0e
Nick Alekhine <nick.alekhine@gmail.com>
2014-04-08 10:50:01 -0400

    add ANSI color codes. work on interface.

--- src/interfacer.java
+++ src/interfacer.java
@@ -7,7 +7,7 @@
  * @version 2014-04-08
  *
  */
-public class interfacer {
+public class Interfacer {
     
     /**
      * Runs the program
@@ -17,9 +17,43 @@ public class interfacer {
      * 
      * @param args - command line arguments
      */
+    // TODO gotta build this out.
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+        Interfacer ui = new Interfacer();
+        try {
+            ui.welcome();
+        }
+        catch (Exception e) {
+            System.out.println("Something unexpected happened...");
+            System.out.println(e);
+        }
+    }
+    
+    
+    /**
+     * The welcome screen. First thing the user sees when they run the program.
+     * 
+     * @author Nick Alekhine
+     * @version 2014-04-08
+     * 
+     */
+    public void welcome() {
+       System.out.println(ANSI.HIGH_INTENSITY 
+                          + "         Welcome to Gradebook.");
+       System.out.println("--------------------------------------- \n");
+       
+       System.out.println(ANSI.SANE + "Options:");
+       System.out.println("    " + ANSI.GREEN + "1" + ANSI.BLACK + 
+                          " - Create an empty Gradebook.");
+       System.out.println("        This option will create a new instance of "
+               + "a Gradebook that is completely empty.");
+       System.out.println("    2 - Create a Gradebook with a file.");
+       System.out.println("        This option will create a new instance of "
+               + "a Gradebook with the supplied text file."
+               + "\n        All content found "
+               + "within the file will be put into the Gradebook if properly "
+               + "formatted.");
+       System.out.println("    3 - Create a Gradebook with a text input.");
     }
 
 }
