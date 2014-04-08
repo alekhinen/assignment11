d374d9bcea362f6e3ca531d6958573aae7d07f38
Nick Alekhine <nick.alekhine@gmail.com>
2014-04-08 10:12:11 -0400

    initialize interface.

---
+++ src/interfacer.java
@@ -0,0 +1,17 @@
+
+/**
+ * This is the main class that users will be able to interface with the 
+ * classes and methods found in this program. 
+ * 
+ * @author Nick Alekhine
+ * @version 2014-04-08
+ *
+ */
+public class interfacer {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
