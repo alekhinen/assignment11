742e1992a5a9d3d85b03faca6135fc354e84dfe4
cclark07 <c.clark1928@gmail.com>
2014-04-08 21:31:04 -0400

    Added some black box tests

--- src/BlackBoxTests.java
+++ src/BlackBoxTests.java
@@ -1,13 +1,251 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import org.junit.Test;
+
+import MyGradeBook.Assignment;
+import MyGradeBook.Course;
+import MyGradeBook.Student;
 
 
 public class BlackBoxTests {
 
-    @Test
-    public void test() {
-        fail("Not yet implemented");
-    }
+	///////////////////////////////////////////////////////////////////////////
+	// FIELDS /////////////////////////////////////////////////////////////////
 
+	/** Student 1 */
+	Student s1;
+	/** Student 2 */
+	Student s2;
+	/** Student 3 */
+	Student s3;
+
+	/** Assignment 1 */
+	Assignment a1;
+	/** Assignment 2 */
+	Assignment a2;
+	/** Assignment 3 */
+	Assignment a3;
+	/** Assignment 4 */
+	Assignment a4;
+	/** Assignment 5 */
+	Assignment a5;
+	/** Assignment 6 */
+	Assignment a6;
+	/** Assignment 7 */
+	Assignment a7;
+	/** Assignment 8 */
+	Assignment a8;
+
+	/** Assignments List 1 */
+	ArrayList<Assignment> aList1;
+	/** Assignments List 2 */
+	ArrayList<Assignment> aList2;
+	/** Assignments List 3 */
+	ArrayList<Assignment> aList3;
+
+	/** Student => Assignments 1 */
+	HashMap<Student, ArrayList<Assignment>> map1;
+
+	/** Course 1 */
+	Course c1;
+
+	/** Course 1 studAssignMap **/
+
+	/** 
+	 * To set values to each variable. 
+	 * */
+	public void reset() {
+		/** Students */
+		this.s1 = 
+				new Student("chperrone", "Charles", "Haydon", "Perrone", 2017);
+		this.s2 = new Student("thmyolk", "Thom", "Mearle", "Yorke", 2017);
+		this.s3 = new Student("nalekhn", "Nick", "Alex", "Alekhine", 2017);
+
+		/** Assignments */
+		this.a1 = new Assignment("Assignment1", 100, 90, .25);
+		this.a2 = new Assignment("Assignment2", 120, 115, .50);
+
+		this.a3 = new Assignment("Assignment1", 100, 45, .25);
+		this.a4 = new Assignment("Assignment2", 120, 80, .50);
+		this.a5 = new Assignment("Assignment3", 100, 90, .25);
+
+		this.a6 = new Assignment("Assignment1", 100, 96, .25);
+		this.a7 = new Assignment("Assignment2", 120, 114, .50);
+		this.a8 = new Assignment("Assignment3", 100, 90, .25);
+
+		/** Populate Assignment Lists */
+		this.aList1 = new ArrayList<Assignment>();
+		this.aList1.add(this.a1);
+		this.aList1.add(this.a2);
+
+		this.aList2 = new ArrayList<Assignment>();
+		this.aList2.add(this.a3);
+		this.aList2.add(this.a4);
+		this.aList2.add(this.a5);
+
+		this.aList3 = new ArrayList<Assignment>();
+		this.aList3.add(this.a6);
+		this.aList3.add(this.a7);
+		this.aList3.add(this.a8);
+
+		this.map1 = new HashMap<Student, ArrayList<Assignment>>();
+		this.map1.put(this.s1, this.aList1);
+		this.map1.put(this.s2, this.aList2);
+		this.map1.put(this.s3, this.aList3);
+
+		this.c1 = this.c1.newGradeBook(map1);
+	}
+
+
+
+
+
+	///////////////////////////////////////////////////////////////////////////
+	// METHODS ////////////////////////////////////////////////////////////////
+
+	/** test the initialize method in Course 
+	 * 
+	 * @author Chris Clark
+	 * @version 2014-04-08
+	 */
+	@Test
+	public void testInitialize() {
+		this.c1 = this.c1.newGradeBook();
+		assertEquals(Course.initialize(), c1);
+	}
+
+	/** test the initializeWithFile method in Course **/
+	@Test
+	public void testInitializeWithFile() {
+		//TODO
+	}
+
+	/** test the initializeWithString method in Course **/
+	@Test
+	public void testInitializeWithString() {
+		//TODO
+	}
+
+	/** test the processFile method in Course **/
+	@Test
+	public void testProcessFile() {
+		//TODO
+	}
+
+	/** test the processString method in Course **/
+	@Test
+	public void testProcessString() {
+		//TODO
+	}
+
+	/** test the changeGrade method in Course
+	 *
+	 * @author Chris Clark
+	 * @version 2014-04-08
+	 */
+	@Test
+	public void testChangeGrade() {
+		reset();
+		assertTrue(c1.changeGrade("Assignment1", "chperrone", 95));
+		assertFalse(c1.changeGrade("Assignment4", "chperrone", 10));
+		assertFalse(c1.changeGrade("Assignment1", "joeblo", 100));
+	}
+
+	/** test the average method in Course
+	 *
+	 * @author Chris Clark
+	 * @version 2014-04-08
+	 */
+	@Test
+	public void testAverage() {
+		reset();
+		assertEquals(c1.average("Assignment1"), 77.0, 0);
+		assertEquals(c1.average("Assignment2"), 85.83, 0.01);
+		assertEquals(c1.average("Assignment3"), 90.0, 0);
+	}
+
+	/** test the median method in Course
+	 *
+	 * @author Chris Clark
+	 * @version 2014-04-08
+	 */
+	@Test
+	public void testMedian() {
+		reset();
+		assertEquals(c1.median("Assignment1"), 90, .0);
+		assertEquals(c1.median("Assignment2"), 95, .0);
+		assertEquals(c1.median("Assignment3"), 90, .0);
+
+		c1.changeGrade("Assignment1", "chperrone", 25);
+
+		assertFalse(c1.median("Assignment1") == 90);
+		assertEquals(c1.median("Assignment1"), 45, .0);
+	}
+
+	/** test the min method in Course
+	 *
+	 * @author Chris Clark
+	 * @version 2014-04-08
+	 */
+	@Test
+	public void testMin() {
+		reset();
+		assertEquals(c1.min("Assignment1"), 45, 0);
+		
+		c1.changeGrade("Assignment1", "thmyolk", 55);
+		assertEquals(c1.min("Assignment1"), 55, .0001);
+	}
+
+	/** test the max method in Course
+	 *
+	 * @author Chris Clark
+	 * @version 2014-04-08
+	 */
+	@Test
+	public void testMax() {
+		reset();
+		assertEquals(c1.max("Assignment1"), 96, 0);
+		
+		c1.changeGrade("Assignment1", "chperrone", 100);
+		assertEquals(c1.max("Assignment1"), 100, .0001);
+	}
+
+	/** test the currentGrade method in Course **/
+	@Test
+	public void testCurrentGrade() {
+		//TODO
+	}
+
+	/** test the currentGrades method in Course **/
+	@Test
+	public void testCurrentGrades() {
+		//TODO
+	}
+
+	/** test the assignmentGrade method in Course **/
+	@Test
+	public void testAssignmentGrade() {
+		//TODO
+	}
+
+	/** test the outputCurrentGrades method in Course **/
+	@Test
+	public void testOutputCurrentGrades() {
+		//TODO
+	}
+
+	/** test the outputAssignmentGrades method in Course **/
+	@Test
+	public void testOutputAssignmentGrades() {
+		//TODO
+	}
+
+	/** test the outputGradebook method in Course **/
+	@Test
+	public void testOutputGradebook() {
+		//TODO
+	}
 }
--- src/MyGradeBook/Student.java
+++ src/MyGradeBook/Student.java
@@ -12,101 +12,101 @@ import java.util.HashMap;
  *
  */
 public class Student {
-    
-    ///////////////////////////////////////////////////////////////////////////
-    // FIELDS /////////////////////////////////////////////////////////////////
-  
-    /** The username of this student */
-    String userName;
-    /** The first name of this student */
-    String firstName;
-    /** The middle name of this student */
-    String middleName;
-    /** The last name of this student */
-    String lastName;
-    /** The year that this student is graduating */
-    int gradYear;
-    
-    
-    
-    
-    
-    ///////////////////////////////////////////////////////////////////////////
-    // CONSTRUCTORS ///////////////////////////////////////////////////////////
-   
-    /**
-     * 
-     * @param userName
-     * @param firstName
-     * @param middleName
-     * @param lastName
-     * @param gradYear
-     */
-    Student(String userName, 
-            String firstName, 
-            String middleName, 
-            String lastName, 
-            int gradYear) {
-        this.userName = userName;
-        this.firstName = firstName;
-        this.middleName = middleName;
-        this.lastName = lastName;
-        this.gradYear = gradYear;
-    }
-    
-    
-    
-    
-    
-    ///////////////////////////////////////////////////////////////////////////
-    // METHODS ////////////////////////////////////////////////////////////////
-    
-    /** overriding the equals method for student class
-     * 
-     * @author Austin Colcord
-     * 
-     * @param other the object to compare to this student
-     * @return boolean true if the other object is equal to this student
-     */
-    @Override
-    public boolean equals(Object other) {
-        if (other instanceof Student) {
-            return this.userName.equals(((Student) other).userName) &&
-                    this.firstName.equals(((Student) other).firstName) &&
-                    this.middleName.equals(((Student) other).middleName) &&
-                    this.lastName.equals(((Student) other).lastName)&&
-                    this.gradYear == ((Student) other).gradYear;
-        }
-        else {
-            return false;
-        }
-    }
-    
-    /** override the hashcode method for student class
-     * 
-     * @author Austin Colcord
-     * 
-     * @return int the hashcode for this student
-     */
-    @Override
-    public int hashCode() {
-        return this.userName.hashCode() +
-                this.firstName.hashCode() +
-                this.middleName.hashCode() +
-                this.lastName.hashCode() +
-                this.gradYear;
-    }
-    
-    /**
-     * override the toString method
-     * 
-     * @author charlesperrone
-     * 
-     * @return this Student's userName
-     */
-    @Override
-    public String toString() {
-        return this.userName;
-    }
-    
+
+	///////////////////////////////////////////////////////////////////////////
+	// FIELDS /////////////////////////////////////////////////////////////////
+
+	/** The username of this student */
+	String userName;
+	/** The first name of this student */
+	String firstName;
+	/** The middle name of this student */
+	String middleName;
+	/** The last name of this student */
+	String lastName;
+	/** The year that this student is graduating */
+	int gradYear;
+
+
+
+
+
+	///////////////////////////////////////////////////////////////////////////
+	// CONSTRUCTORS ///////////////////////////////////////////////////////////
+
+	/**
+	 * 
+	 * @param userName
+	 * @param firstName
+	 * @param middleName
+	 * @param lastName
+	 * @param gradYear
+	 */
+	Student(String userName, 
+			String firstName, 
+			String middleName, 
+			String lastName, 
+			int gradYear) {
+		this.userName = userName;
+		this.firstName = firstName;
+		this.middleName = middleName;
+		this.lastName = lastName;
+		this.gradYear = gradYear;
+	}
+
+
+
+
+
+	///////////////////////////////////////////////////////////////////////////
+	// METHODS ////////////////////////////////////////////////////////////////
+
+	/** overriding the equals method for student class
+	 * 
+	 * @author Austin Colcord
+	 * 
+	 * @param other the object to compare to this student
+	 * @return boolean true if the other object is equal to this student
+	 */
+	@Override
+	public boolean equals(Object other) {
+		if (other instanceof Student) {
+			return this.userName.equals(((Student) other).userName) &&
+					this.firstName.equals(((Student) other).firstName) &&
+					this.middleName.equals(((Student) other).middleName) &&
+					this.lastName.equals(((Student) other).lastName)&&
+					this.gradYear == ((Student) other).gradYear;
+		}
+		else {
+			return false;
+		}
+	}
+
+	/** override the hashcode method for student class
+	 * 
+	 * @author Austin Colcord
+	 * 
+	 * @return int the hashcode for this student
+	 */
+	@Override
+	public int hashCode() {
+		return this.userName.hashCode() +
+				this.firstName.hashCode() +
+				this.middleName.hashCode() +
+				this.lastName.hashCode() +
+				this.gradYear;
+	}
+
+	/**
+	 * override the toString method
+	 * 
+	 * @author charlesperrone
+	 * 
+	 * @return this Student's userName
+	 */
+	@Override
+	public String toString() {
+		return this.userName;
+	}
+
 }
