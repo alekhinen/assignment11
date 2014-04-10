752133c7a487be1b63e626742d3b313b77442dfb
Austin Colcord <ajdcolcord@gmail.com>
2014-04-10 12:19:38 -0400

    FIX THE BLACKBOX TESTS CHRIS

--- src/BlackBoxTests.java
+++ src/BlackBoxTests.java
@@ -77,6 +77,7 @@ public class BlackBoxTests {
         }
         catch (FileNotFoundException e) {
             // To catch/test expected exceptions
+            assertTrue(false);
         }
     }
 
@@ -115,6 +116,7 @@ public class BlackBoxTests {
         }
         catch (Exception e) {
             // To catch/test expected exceptions
+            assertTrue(false);
         }
     }
 
